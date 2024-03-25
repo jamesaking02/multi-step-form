866cf503ff83e2201035968744069bc4e7e110ff
jamesaking02 <jamesaking02@outlook.com>
2024-03-25 15:48:34 -0700

    added code for switch

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const formStep4 = document.querySelector(".step4")
 const formButton = document.querySelectorAll(".form-button")
 const asideSteps = document.querySelectorAll(".step__number")
 const planOptions = document.querySelectorAll(".option")
+const yearlyDeal = document.querySelectorAll(".yearlydeal")
 
 document.querySelector("form").addEventListener('click', function(e) {
   e.preventDefault()
@@ -61,5 +62,25 @@ document.querySelector("form").addEventListener('click', function(e) {
       
     })
   }
+
+  if (e.target.classList.contains("switch")) {
+    this.querySelector(".switch__circle").classList.toggle("yearlyplan")
+    if (this.querySelector(".switch__circle").classList.contains("yearlyplan")) {
+      this.querySelector(".monthly").classList.remove("active")
+      this.querySelector(".yearly").classList.add("active")
+
+      yearlyDeal.forEach((deal) => {
+        deal.classList.add("visible")
+      })
+    } else {
+      this.querySelector(".monthly").classList.add("active")
+      this.querySelector(".yearly").classList.remove("active")
+
+      yearlyDeal.forEach((deal) => {
+        deal.classList.remove("visible")
+      })
+    }
+  }
 })
+
 
