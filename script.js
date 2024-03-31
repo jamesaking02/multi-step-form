7bc5a8343f641e39d32ffc3e99784e81a2d7b0a5
jamesaking02 <jamesaking02@outlook.com>
2024-03-30 17:04:25 -0700

    fixed add-on code for the summary

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const asideSteps = document.querySelectorAll(".step__number")
 const planOptions = document.querySelectorAll(".option")
 const yearlyDeal = document.querySelectorAll(".yearlydeal")
 const addOns = document.querySelectorAll(".add-on")
+let checkedNum = 0
 
 document.querySelector("form").addEventListener('click', function(e) {
   if (e.target.classList.contains("nextstep") || e.target.classList.contains("goback")
@@ -81,11 +82,8 @@ document.querySelector("form").addEventListener('click', function(e) {
 
   addOns.forEach((addOn) => {
     addOn.addEventListener('change', (e) => {
-      if (e.target.checked) {
-        e.target.parentElement.parentElement.classList.add("checked")
-      } else {
-        e.target.parentElement.parentElement.classList.remove("checked")
-      }
+      e.target.checked ? e.target.parentElement.parentElement.classList.add("checked") 
+      : e.target.parentElement.parentElement.classList.remove("checked")
     })
     
   })
@@ -288,91 +286,117 @@ function goBack() {
 function createSummary() {
   const summaryPlanType = document.querySelector(".plan-type")
   const summaryPlanPrice = document.querySelector(".plan-price")
-  const summaryAddOns = document.querySelector(".summary__add-ons")
+  const summaryAddOnPrice = document.querySelectorAll(".summary-add-on__price")
   let yearPlanSelected = document.querySelector(".switch__circle").classList.contains("yearlyplan")
+  const totalLabel = document.querySelector(".total-label")
+  const total = document.querySelector(".total").textContent
+  let totalPrice = 0
+  
+  yearPlanSelected ? totalLabel.textContent = "Total (per year)" : totalLabel.textContent = "Total (per month)"
 
   planOptions.forEach((option) => {
     if (option.classList.contains("selected") && yearPlanSelected) {
       if (option.dataset.planOption === "arcade") {
 
         summaryPlanType.textContent = "Arcade (Yearly)"
-        summaryPlanPrice.textContent = "$90/mo"
-
+        summaryPlanPrice.textContent = "$90/yr"
+        totalPrice += parseInt(summaryPlanPrice.textContent.substring(1, 3))
       } else if (option.dataset.planOption === "advanced") {
 
         summaryPlanType.textContent = "Advanced (Yearly)"
-        summaryPlanPrice.textContent = "$120/mo"
+        summaryPlanPrice.textContent = "$120/yr"
+        totalPrice += parseInt(summaryPlanPrice.textContent.substring(1, 4))
 
       } else if (option.dataset.planOption === "pro") {
 
         summaryPlanType.textContent = "Pro (Yearly)"
-        summaryPlanPrice.textContent = "$150/mo"
-
+        summaryPlanPrice.textContent = "$150/yr"
+        totalPrice += parseInt(summaryPlanPrice.textContent.substring(1, 4))
       }
     } else if (option.classList.contains("selected")) {
       if (option.dataset.planOption === "arcade") {
 
         summaryPlanType.textContent = "Arcade (Monthly)"
         summaryPlanPrice.textContent = "$9/mo"
+        totalPrice += parseInt(summaryPlanPrice.textContent.substring(1, 2))
 
       } else if (option.dataset.planOption === "advanced") {
 
         summaryPlanType.textContent = "Advanced (Monthly)"
         summaryPlanPrice.textContent = "$12/mo"
+        totalPrice += parseInt(summaryPlanPrice.textContent.substring(1, 3))
 
       } else if (option.dataset.planOption === "pro") {
 
         summaryPlanType.textContent = "Pro (Monthly)"
         summaryPlanPrice.textContent = "$15/mo"
+        totalPrice += parseInt(summaryPlanPrice.textContent.substring(1, 3))
 
       }
+      console.log(totalPrice)
     } 
   })
 
+  
+
+addOns.forEach((addOn) => {
+  let anyChecked = false
+
   addOns.forEach((addOn) => {
-    if (!addOn.classList.contains("checked")) {
-        const summaryAddon1 = document.querySelector("#summary__addon1")
-        const addOnName = document.querySelector("#addon1__name")
+    addOn.classList.contains("checked") ? anyChecked = true : false
+  })
+  
+  if (!anyChecked) {
+    const summaryAddon1 = document.querySelector("#summary__addon1")
+    summaryAddon1.children[0].textContent = "No Add-ons"
+  }
 
-        summaryAddon1.children[0].textContent = "No Add-ons"
+  if (addOn.dataset.addonType === "online service" && addOn.classList.contains("checked")) {
+    const summaryAddon1 = document.querySelector("#summary__addon1")
+    const addOnName = document.querySelector("#addon1__name")
+    const addOnPrice = document.querySelector("#addon1__price")
+          
+    summaryAddon1.children[0].textContent = addOnName.textContent
+    summaryAddon1.children[1].textContent = addOnPrice.textContent
 
-      } else {
-        addOns.forEach((addOn) => {
-          if (addOn.dataset.addonType === "online service") {
-            const summaryAddon1 = document.querySelector("#summary__addon1")
-            const addOnName = document.querySelector("#addon1__name")
-            const addOnPrice = document.querySelector("#addon1__price")
-            
-            summaryAddon1.children[0].textContent = addOnName.textContent
-            summaryAddon1.children[1].textContent = addOnPrice.textContent
-            
-          } else if (addOn.dataset.addonType === "larger storage") {
-            const summaryAddon2 = document.querySelector("#summary__addon2")
-            const addOnName = document.querySelector("#addon2__name")
-            
-            summaryAddon2.children[0].textContent = addOnName.textContent
-            summaryAddon2.children[1].textContent = "+2/mo"
+  } else if (addOn.dataset.addonType === "online service") {
+    const summaryAddon1 = document.querySelector("#summary__addon1")
 
-          } else if (addOn.dataset.addonType === "customizable profile") {
-            const summaryAddon2 = document.querySelector("#summary__addon3")
-            const addOnName = document.querySelector("#addon3__name")
-            
-            summaryAddon2.children[0].textContent = addOnName.textContent
-            summaryAddon2.children[1].textContent = "+2/mo"
-          }
-        })
-      }
-    })
+    summaryAddon1.children[0].textContent = ""
+    summaryAddon1.children[1].textContent = ""
+  }
+
+  if (addOn.dataset.addonType === "larger storage"  && addOn.classList.contains("checked")) {
+    const summaryAddon2 = document.querySelector("#summary__addon2")
+    const addOnName = document.querySelector("#addon2__name")
+    const addOnPrice = document.querySelector("#addon2__price")
+          
+    summaryAddon2.children[0].textContent = addOnName.textContent
+    summaryAddon2.children[1].textContent = addOnPrice.textContent
+    
+  } else if (addOn.dataset.addonType === "larger storage") {
+    const summaryAddon2 = document.querySelector("#summary__addon2")
+
+    summaryAddon2.children[0].textContent = ""
+    summaryAddon2.children[1].textContent = ""
+  }
+
+  if (addOn.dataset.addonType === "customizable profile"  && addOn.classList.contains("checked")) {
+    const summaryAddon3 = document.querySelector("#summary__addon3")
+    const addOnName = document.querySelector("#addon3__name")
+    const addOnPrice = document.querySelector("#addon3__price")
+          
+    summaryAddon3.children[0].textContent = addOnName.textContent
+    summaryAddon3.children[1].textContent = addOnPrice.textContent
+
+
+  } else if (addOn.dataset.addonType === "customizable profile") {
+    const summaryAddon3 = document.querySelector("#summary__addon3")
+
+    summaryAddon3.children[0].textContent = ""
+    summaryAddon3.children[1].textContent = ""
+  }
+    
+})
+    
 }
-
-// let addOn1 = document.createElement("p")
-// let addon1Price = document.createElement("p")
-// let addOn1Content = document.createTextNode("Online service")
-// let addOn1PriceContent = document.createTextNode("+$1/mo")
-
-// addOn1.appendChild(addOn1Content)
-// addon1Price.appendChild(addOn1PriceContent)
-
-
-// const summaryAddon1 = document.querySelector("#summary__addon1")
-// console.log(summaryAddon1.children[0])
